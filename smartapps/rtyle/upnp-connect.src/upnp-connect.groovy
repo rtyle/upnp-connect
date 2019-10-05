a04d1eb6c3042dec1de5fd92e455447e6757685d
Ross Tyler <rossetyler@gmail.com>
2019-10-05 10:26:41 -0700

    log levels

--- smartapps/rtyle/upnp-connect.src/upnp-connect.groovy
+++ smartapps/rtyle/upnp-connect.src/upnp-connect.groovy
@@ -36,6 +36,18 @@ preferences {
 	input 'search', 'bool', defaultValue: true, title: 'Request a discovery search'
 	input 'create', 'bool', defaultValue: true, title: 'Create discovered devices'
 	input 'prefix', 'text', defaultValue: 'UPnP', required: false, title: 'Device label prefix'
+    input 'logLevel', 'number', defaultValue: '1', title: 'Log level [-1..4]', range: '-1..4'
+}
+
+private int getTrace() {0}
+private int getDebug() {1}
+private int getInfo	() {2}
+private int getWarn	() {3}
+private int getError() {4}
+private void log(int level, String message, Throwable throwable = null) {
+	if (level > logLevel) {
+		log."${['trace', 'debug', 'info', 'warn', 'error'][level]}" message, throwable
+    }
 }
 
 private String getNamespace() {
@@ -84,11 +96,11 @@ private def decodeMap(String serialization, String major = '\\s*,\\s*', String m
 
 void ssdpPathResponse(physicalgraph.device.HubResponse hubResponse) {
 	def message = parseLanMessage(hubResponse.description)
-	// log.debug "ssdpPathResponse: $message.body"
+	// log debug, "ssdpPathResponse: $message.body"
 	def xml = parseXml(message.body)
 	def device = xml.device
 	String udn = decodeMap(device.UDN.text()).uuid
-	log.debug "ssdpPathResponse: udn=$udn"
+	log debug, "ssdpPathResponse: udn=$udn"
 
 	if (rememberedDevice."$udn") {
 		def remembered = rememberedDevice."$udn"
@@ -108,20 +120,20 @@ void ssdpPathResponse(physicalgraph.device.HubResponse hubResponse) {
 		physicalgraph.app.DeviceWrapper macChild = getChildDevice mac
 		if (!macChild) {
 			def label = name + ' ' + mac
-			log.debug "ssdpPathResponse: addChildDevice $namespace, $name, $mac, $hubResponse.hubId [label: $label, completedSetup: true]"
+			log debug, "ssdpPathResponse: addChildDevice $namespace, $name, $mac, $hubResponse.hubId [label: $label, completedSetup: true]"
 			macChild = addChildDevice namespace, name, mac, hubResponse.hubId, [label: label, completedSetup: true]
 		}
 
 		// create the UDN identified child, if we support its URN and needed
 		String urn = remembered.ssdpTerm.urn
-		log.debug("ssdpPathResponse: urn=$urn")
+		log debug, "ssdpPathResponse: urn=$urn"
 		if (urnToDeviceHandler."$urn") {
 			def deviceHandler = urnToDeviceHandler."$urn"
-			log.debug("ssdpPathResponse: urn=$urn device hander $deviceHandler")
+			log debug, "ssdpPathResponse: urn=$urn device hander $deviceHandler"
 			physicalgraph.app.DeviceWrapper udnChild = getChildDevice udn
 			if (!udnChild) {
 				String label = (prefix && !prefix.isEmpty() ? prefix + ' ' : '') + device.friendlyName.text()
-				log.debug "ssdpPathResponse: addChildDevice $deviceHandler.namespace, $deviceHandler.name, $udn, hubResponse.hubId, [label: $label, data: [networkAddress: $remembered.networkAddress, deviceAddress: $remembered.deviceAddress, ssdpPath: $remembered.ssdpPath, description: $hubResponse.description]]"
+				log debug, "ssdpPathResponse: addChildDevice $deviceHandler.namespace, $deviceHandler.name, $udn, hubResponse.hubId, [label: $label, data: [networkAddress: $remembered.networkAddress, deviceAddress: $remembered.deviceAddress, ssdpPath: $remembered.ssdpPath, description: $hubResponse.description]]"
 				udnChild = addChildDevice deviceHandler.namespace, deviceHandler.name, udn, hubResponse.hubId, [
 					data			: [
 						description		: hubResponse.description,
@@ -135,7 +147,7 @@ void ssdpPathResponse(physicalgraph.device.HubResponse hubResponse) {
 				udnChild.install()
 			}
 		} else {
-			log.error("ssdpPathResponse: urn=$urn has no device handler")
+			log error, "ssdpPathResponse: urn=$urn has no device handler"
 		}
 	}
 }
@@ -144,13 +156,13 @@ private void ssdpDiscovered(physicalgraph.app.EventWrapper e) {
 	def discovered = parseLanMessage e.description
 	discovered.ssdpUSN = decodeMap discovered.ssdpUSN, '\\s*::\\s*'
 	discovered.ssdpTerm = decodeMap discovered.ssdpTerm
-	log.debug "ssdpDiscovered: $discovered"
+	log debug, "ssdpDiscovered: $discovered"
 
 	String urn = discovered.ssdpUSN.urn
 	if (!urnToDeviceHandler."$urn") {
 		// ignore discovered services
 		// and discovered devices we can't handle (we shouldn't get these)
-		log.debug("ssdpDiscovered: ignore $urn")
+		log debug, "ssdpDiscovered: ignore $urn"
 		return
 	}
 
@@ -159,12 +171,12 @@ private void ssdpDiscovered(physicalgraph.app.EventWrapper e) {
 
 	physicalgraph.app.DeviceWrapper udnChild = getChildDevice udn
 	if (udnChild) {
-		log.debug "ssdpDiscovered: (getChildDevice $udn).update $discovered.networkAddress $discovered.deviceAddress"
+		log debug, "ssdpDiscovered: (getChildDevice $udn).update $discovered.networkAddress $discovered.deviceAddress"
 		udnChild.update discovered.networkAddress, discovered.deviceAddress	
 	} else {
 		if (create) {
 			String target = decodeNetworkAddress(discovered.networkAddress) + ':' + decodeDeviceAddress(discovered.deviceAddress)
-			log.debug "ssdpDiscovered: GET http://$target${discovered.ssdpPath}"
+			log debug, "ssdpDiscovered: GET http://$target${discovered.ssdpPath}"
 			sendHubCommand new physicalgraph.device.HubAction(
 				"GET ${discovered.ssdpPath} HTTP/1.1\r\nHOST: $target\r\n\r\n",
 				physicalgraph.device.Protocol.LAN,
@@ -179,7 +191,7 @@ private void ssdpDiscover() {
 	if (search) {
 		List hubActions = [];
 		urnToDeviceHandler.each {urn, notUsed ->
-			log.debug "ssdpDiscover: hubAction lan discover urn:${urn}"
+			log debug, "ssdpDiscover: hubAction lan discover urn:${urn}"
 			hubActions.add new physicalgraph.device.HubAction('lan discovery urn:' + urn, physicalgraph.device.Protocol.LAN)
 		}
 		sendHubCommand hubActions, 4000	// perform hubActions with a delay between them
@@ -189,25 +201,25 @@ private void ssdpDiscover() {
 private void ssdpSubscribe() {
 	urnToDeviceHandler.each {urn, deviceHandler ->
 		// subscribe to event by name (ssdpTerm) and (dot) value (urn:$urn)
-		log.debug "ssdpSubscribe: subscribe ssdpTerm.urn:$urn"
+		log debug, "ssdpSubscribe: subscribe ssdpTerm.urn:$urn"
 		subscribe location, 'ssdpTerm' + '.urn:' + urn, 'ssdpDiscovered'
 	}
 }
 
 void updated() {
-	log.debug "updated $settings"
+	log debug, "updated $settings"
 	ssdpDiscover()
 }
 
 void installed() {
-	log.debug "installed $settings"
+	log debug, "installed $settings"
 	ssdpSubscribe()
 	ssdpDiscover()
 }
 
 void uninstalled() {
 	getChildDevices().each({child ->
-		log.debug "uninstalled: deleteChildDevice $child.deviceNetworkId"
+		log debug, "uninstalled: deleteChildDevice $child.deviceNetworkId"
 		child.uninstall()
 		deleteChildDevice child.deviceNetworkId
 	});
